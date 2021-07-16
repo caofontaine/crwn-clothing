9cbd771089a5c8016db8a5e8fd93d0935d48ab97
caofontaine <benjamin.cao88@gmail.com>
2021-07-16 14:46:40 -0400

    updating our app with a shop page, collection preview component, and collection item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-//higher order component - function that takes a component as an arg and returns a modified component
+
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
